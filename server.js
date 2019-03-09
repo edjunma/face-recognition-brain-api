44c13f59f33dcd214352e0d38636baf74845e301
edjunma <junwenma@buffalo.edu>
2019-03-09 14:59:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.post("./imgurl", (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("app is running on port 3000 ${process.env.PORT}");
+  console.log(`app is running on port ${process.env.PORT}`);
 });

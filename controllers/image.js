30cbae51e64cca390daafebd90d6fa037226e52c
edjunma <junwenma@buffalo.edu>
2019-03-09 14:42:32 -0500

    Updated and fixed some code

--- controllers/image.js
+++ controllers/image.js
@@ -1,31 +1,32 @@
-const Clarifai = require('clarifai');
+const Clarifai = require("clarifai");
 
 // You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: '147046a299a541b4902c4001df6535a2'
+  apiKey: "147046a299a541b4902c4001df6535a2"
 });
 
 const handleApiCall = (req, res) => {
   app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .predict("147046a299a541b4902c4001df6535a2", req.body.input)
     .then(data => {
       res.json(data);
     })
-    .catch(err => res.status(400).json('unable to work with API'));
-}
+    .catch(err => res.status(400).json("Unable to connect to API"));
+};
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-}
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json("unable to get entries"));
+};
 
 module.exports = {
-  handleImage: handleImage
-  handleApiCall: handleApiCall
-}
+  handleImage,
+  handleApiCall
+};
--- server.js
+++ server.js
@@ -1,45 +1,45 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const img = require("./controllers/img");
 
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: true
   }
 });
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('it is working!') })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('./image', (req, res) => { image.handleImage(req, res, db)})
-app.post('./imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get("/", (req, res) => {
+  res.send("it is working!");
+});
+app.post("/signin", (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put("./img", (req, res) => {
+  img.handleImage(req, res, db);
+});
+app.post("./imgurl", (req, res) => {
+  img.handleApiCall(req, res);
+});
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('app is running on port 3000 ${process.env.PORT}');
-})
-
-
-/*
-
-/ --> res = this is working
-/ signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user
-
-*/
+  console.log("app is running on port 3000 ${process.env.PORT}");
+});
